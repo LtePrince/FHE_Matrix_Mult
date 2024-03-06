37a16dd74d915d68d02185c9ac19eb3706ab25be
Adolph <574699248@qq.com>
2024-03-06 11:42:16 +0800

    添加了未做scale处理的Replicate1D和Sum1D,对Mat_dim_process进行了修改，使其前后scale保持基本一致

--- src/test2.cpp
+++ src/test2.cpp
@@ -81,14 +81,25 @@ void Mat_dim_process(Cipher_Matrix& m, CKKSEncoder& encoder, Evaluator &evaluato
         parms_id_type last_parms_id = m.m.parms_id();
         evaluator.mod_switch_to_inplace(mask1, last_parms_id);
         evaluator.mod_switch_to_inplace(mask2, last_parms_id);
+        cout << endl;
+        cout << "    + Scale of m before multiply: " << log2(m.m.scale()) << " bits" << endl;
         evaluator.multiply_plain(m.m, mask1, row_data);
-        evaluator.rescale_to_inplace(row_data, last_parms_id);
+        cout << "    + Scale of r before rescale: " << log2(row_data.scale()) << " bits" << endl;
+        evaluator.rescale_to_next_inplace(row_data);
+        cout << "    + Scale of r after rescale: " << log2(row_data.scale()) << " bits" << endl;
 
+        cout << endl;
         evaluator.multiply_plain_inplace(m.m, mask2);
-        evaluator.rescale_to_inplace(m.m, last_parms_id);
+        cout << "    + Scale of m before rescale: " << log2(m.m.scale()) << " bits" << endl;
+        evaluator.rescale_to_next_inplace(m.m);
+        cout << "    + Scale of m after rescale: " << log2(m.m.scale()) << " bits" << endl;
 
+        cout << endl;
         evaluator.rotate_vector_inplace(row_data, -step, gal_keys);
+        cout << "    + Scale of after rotate: " << log2(row_data.scale()) << " bits" << endl;
+
         evaluator.add_inplace(m.m, row_data);
+        cout << "    + Scale of after add: " << log2(m.m.scale()) << " bits" << endl;
     }
 }
 
@@ -276,9 +287,62 @@ void RotateAlign(Cipher_Matrix& m, Cipher_Matrix& destination, CKKSEncoder& enco
     }
 }
 
-void Replicate1D()
+void Replicate1D(Cipher_Matrix& m, Cipher_Matrix& destination, CKKSEncoder& encoder, Evaluator& evaluator, GaloisKeys& gal_keys, int dim, int d_dim, int D0, int D1, int slot_count, int scale)
+{
+    destination.m = m.m;
+    destination.col[0] = m.col[0];
+    destination.col[1] = m.col[1];
+    destination.row[0] = m.row[0];
+    destination.row[1] = m.row[1];
+
+    Mat_Extern(destination, encoder, evaluator, gal_keys, slot_count, scale, D0, D1);
+    int D_dim = (dim == 1) ? D1 : D0;
+    int k_max = log(D_dim / d_dim) / log(2);
+    for (int k = 1; k <= k_max; k++)
+    {
+        Plaintext mask;
+        vector<double> input(slot_count, 1.0);
+        encoder.encode(input, scale, mask);
+
+        Cipher_Matrix rotate_data;
+        evaluator.multiply_plain(destination.m, mask, rotate_data.m);
+
+        Rotate1D(rotate_data, encoder, evaluator, gal_keys, dim, k* d_dim, slot_count, scale);
+        
+        evaluator.add_inplace(destination.m, rotate_data.m);
+    }
+}
+
+void Sum1D(Cipher_Matrix& m, CKKSEncoder& encoder, Evaluator& evaluator, GaloisKeys& gal_keys, int dim, int d_dim, int D0, int D1, int slot_count, int scale)
 {
 
+    //未作scale和parm_id的调整
+    for (int k = log(D1 / d_dim) / log(2); k > 0; k--)
+    {
+        Plaintext mask;
+        encoder.encode(1.0, scale, mask);
+
+        Cipher_Matrix rotate_data;
+        rotate_data.col[0] = m.col[0];
+        rotate_data.col[1] = m.col[1];
+        rotate_data.row[0] = m.row[0];
+        rotate_data.row[1] = m.row[1];
+
+        parms_id_type last_parms_id = m.m.parms_id();
+        evaluator.mod_switch_to_inplace(mask, last_parms_id);
+        evaluator.multiply_plain(m.m, mask, rotate_data.m);
+        evaluator.rescale_to_next_inplace(rotate_data.m);
+
+        Rotate1D(rotate_data, encoder, evaluator, gal_keys, dim, k*d_dim, slot_count, scale);
+        cout << "m scale: " << m.m.scale() << endl;
+        cout << "rotate_data scale: " << rotate_data.m.scale() << endl;
+
+        cout << "m parm_id: " << last_parms_id << endl;
+        cout << "rotate_data parm_id: " << rotate_data.m.parms_id() << endl;
+        cout << endl;
+        
+        evaluator.add_inplace(m.m, rotate_data.m);
+    }
 }
 
 int main()
@@ -287,7 +351,7 @@ int main()
 
     size_t poly_modulus_degree = 8192*2;
     parms.set_poly_modulus_degree(poly_modulus_degree);
-    parms.set_coeff_modulus(CoeffModulus::Create(poly_modulus_degree, { 60, 40, 40, 40, 40, 40, 40, 40, 60 }));
+    parms.set_coeff_modulus(CoeffModulus::Create(poly_modulus_degree, { 60, 30, 30, 30, 30, 30, 30, 30, 60 }));
 
     double scale = pow(2.0, 30);
 
@@ -316,15 +380,20 @@ int main()
     Init_Matrix(m1, encoder, encryptor, slot_count, scale);
     //Init_Matrix(m2, encoder, encryptor, slot_count, scale);
 
+    cout << "    + Scale of M_d_p before rescale: " << log2(m1.m.scale()) << " bits" << endl;
     Mat_dim_process(m1, encoder, evaluator, gal_keys, slot_count, scale);
+    cout << "    + Scale of M_d_p after rescale: " << log2(m1.m.scale()) << " bits" << endl;
     //Mat_dim_process(m2, encoder, evaluator, gal_keys, slot_count, scale);
 
     //cout << "Rotate1D:" << endl;
     //Rotate1D(m1, encoder, evaluator, gal_keys, 0, 1, slot_count, scale);
 
-    Cipher_Matrix r_m;
-    cout << "RotateAlign:" << endl;
-    RotateAlign(m1, r_m, encoder, evaluator, gal_keys, 1, slot_count, scale);
+    //Cipher_Matrix r_m;
+    //cout << "RotateAlign:" << endl;
+    //RotateAlign(m1, r_m, encoder, evaluator, gal_keys, 1, slot_count, scale);
+
+    //cout << "Sum1D:" << endl;
+    //Sum1D(m1, encoder, evaluator, gal_keys, 1, 2, m1.col[1], m1.row[1], slot_count, scale);
 
     //cout << "Extern:" << endl;
     //Mat_Extern(m1, encoder, evaluator, gal_keys, slot_count, scale, 8, 8);
